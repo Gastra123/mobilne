6d6202360f0700f9c6b624d4bd595c26e1b311c4
Gastra123 <52671454+Gastra123@users.noreply.github.com>
2019-07-08 18:37:09 +0200

    Add files via upload

---
+++ question.ts
@@ -0,0 +1,8 @@
+export class Question {
+    questionText: string;
+    answers: string[] = [];
+    answerIndex1: number;
+    answerIndex2: number;
+    image:string;
+    
+}
---
+++ quizresult.ts
@@ -0,0 +1,6 @@
+export class QuizResult {
+    playerName: string;
+    datePlayed: Date;
+    totalTime: number;
+    score: number;
+}
